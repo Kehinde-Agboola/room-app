5bc57875dc5dda5c2ebe7171b3b8a3bb06e6a53d
Your Name <kehindeagboola52@gmail.com>
2022-10-11 10:23:31 +0100

    setting up the files

---
+++ src/component/Nav/data.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const data = () => [
+  {
+    tittle: "home",
+    index: "Home",
+  },
+  {
+    tittle: "shop",
+    index: "Shop",
+  },
+  {
+    tittle: "about",
+    index: "About",
+  },
+  {
+    tittle: "contact",
+    index: "Contact",
+  },
+];
